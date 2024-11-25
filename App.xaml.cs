20fb2c0431af2e84285ea5b92b32965567dab23e
DARIA-ȘTEFANA GHIREAN <daria.ghirean@stud.ubbcluj.ro>
2024-11-25 15:53:26 +0200

    Laboratorul 8, dar cu cateva erori

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,23 @@
-﻿namespace GhireanDariaLab7
+﻿using System;
+using GhireanDariaLab7.Data;
+using System.IO;
+
+
+namespace GhireanDariaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database; 
+        public static ShoppingListDatabase Database 
+        {
+            get { if (database == null)
+                    {
+                        database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                    }
+                
+                return database; 
+            }
+        }
         public App()
         {
             InitializeComponent();
